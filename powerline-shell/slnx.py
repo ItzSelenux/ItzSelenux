6331699eb08c49fa9c89c2a3b88265023c5d4528
ItzSelenux from Desktop PC <ItsZariep@PC>
2022-08-02 20:56:56 -0500

    Changes to be committed: 	new file:   powerline-shell/config.json 	new file:   powerline-shell/slnx.py 	new file:   powerline-shell/theme.py 	modified:   tcshrc

---
+++ powerline-shell/slnx.py
@@ -0,0 +1,16 @@
+{
+  "segments": [
+    "cwd",
+    "git",
+    {
+      "type": "stdout",
+      "command": ["echo", "ItsZariep"],
+      "fg_color": 22,
+      "bg_color": 161
+    },
+    {
+      "type": "env",
+      "var": "DOCKER_MACHINE_NAME"
+    },
+  ]
+}
