c6fea7762101509a24e66e158519e0e102d35dd7
ItzSelenux <ItzSelenux@zariep.com>
2023-11-24 02:26:22 -0600

    update

---
+++ st-flexipatch/selenium_single.h
@@ -0,0 +1,33 @@
+/* Selenium-colorscheme for st by ItzSelenux
+ * github.com/ItzSelenux/selenium-colorscheme */
+/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#3a3a3a",
+	"#a66966",
+	"#91a666",
+	"#a69c66",
+	"#6677a6",
+	"#9a66a6",
+	"#66a6aa",
+	"#a6a6a6",
+
+
+	/* 8 bright colors */
+	"#3a3a3a",
+	"#a66966",
+	"#91a666",
+	"#a69c66",
+	"#6677a6",
+	"#9a66a6",
+	"#66a6aa",
+	"#a6a6a6",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"#2a2a2a", /* default foreground colour */
+	"#a6a6a6", /* default background colour */
+};
